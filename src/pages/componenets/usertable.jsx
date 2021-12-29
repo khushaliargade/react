f95364b7ff0b9f8eef4a8cf61077f90c8452966e
Snehal11599 <snehalc11599@gmail.com>
2021-12-29 03:36:23 -0800

    login api

--- src/pages/componenets/usertable.jsx
+++ src/pages/componenets/usertable.jsx
@@ -1,56 +1,80 @@
 import React, { useState, useEffect } from 'react'
 import { DataGrid } from '@material-ui/data-grid'
 
-const HandleDelete = (id) => {
-    let url = 'http://localhost:8080/test/hello/delete/' + id;
-    fetch(url)
-        .then(resp => resp.json())
-        .then(resp => UserTable())
+
+//delelte
+const  HandleDelete=(id) =>{
+  let url='http://localhost:8080/test/hello/delete/'+id;
+  fetch(url)
+  .then(resp=>resp.json())
+  .then(resp=>UserTable())
 }
+
+const HandleUpdate =() => {
+    let url='http://localhost:8080/test/hello/update';
+    fetch(url)
+  .then(resp=>resp.json())
+  .then(resp=>UserTable())
+}
+
+
 const columns = [
     { field: 'id', headerName: 'ID' },
     { field: 'firstname', headerName: 'First Name' },
     { field: 'lastname', headerName: 'Last Name' },
     { field: 'email', headerName: 'Email' },
     { field: 'password', headerName: 'Password' },
-    {
-        field: 'Action',
-        headerName: 'DeleteAction',
-        renderCell: (params) => {
-            return (
-                <button
-                    color="primary"
-                    variant="contained"
-                    onClick={() => HandleDelete(params.id)} >Delete</button>
-            );
-        }
+    {field:'Action',
+    headerName:'DeleteAction',
+    renderCell: (params) => {
+       return (
+        <button style={{backgroundcolor: "red" ,color:"black"}}
+        
+        onClick={()=>HandleDelete(params.id)}
+        >Delete</button>
+        
+       );
+       }
+    },
+
+      {field:'Action2',
+      headerName:'DeleteUpdate',
+    renderCell: (id) => {
+       return (
+        <button 
+        color= "primary"
+        variant="contained"
+        onClick={()=>HandleUpdate(id)}
+        >Update</button>
+        
+       );
+       }
     }
 
 ]
 const UserTable = () => {
     const [tableData, setTableData] = useState([])
     useEffect(() => {
-
-        if(tableData=='')
-         {
+      if(tableData=='')
+      {
         fetch("http://localhost:8080/test/hello/getusers")
-
+      
             .then((data) => data.json())
-            .then((data) => setTableData(data))
-        }
-    })
+            .then((data) => setTableData(data))}
+          })
+    
 
-    return (
+return (
         <div style={{ height: 700, width: '100%' }}>
             <h2>This is user table</h2>
             <DataGrid
                 rows={tableData}
                 columns={columns}
                 pageSize={10}
-                checkboxSelection /> ,
-
+                checkboxSelection /> 
+                
         </div>
-    )
-}
+      )
+    } 
 
 export default UserTable
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React , { useState } from 'react'
 import loginImg from "../assets/login.png"
 import "./componenets/style.css"
-import { Link } from "react-router-dom";
-export class Login extends React.Component {
-    render() {
+
+  function Login() {
+      const [ email , setEmail] =useState("");
+      const [ password , setPassword] =useState("");
+       
+
+     async function Log() {
+          console.warn("data" ,email,password)
+          let item={email,password}
+          let result= await fetch('http://localhost:8080/test/hello/validateEmail/@praju.com',{
+              method:'POST',
+              headers:{
+                  "Content-Type":"application/json",
+                  "Accept":"application/json"
+              },
+              body:JSON.stringify(item)
+          });
+          result = await result.json()
+          console.log("result", result);
+
+      }
+    
         return (
             <div>
                 <img src={loginImg} alt="" />
                 <form>
-                    <input type='email' name='email' placeholder='email...' required />
-                    <input type='password' name='pwd' placeholder='password...' required />
-                    <Link to="/home" class="button">Login</Link>
+                    <input type='email' name='email' onChange={(e) => setEmail(e.target.value)} placeholder='email...' required   />
+                    <input type='password' name='pwd' onChange={(e) => setPassword(e.target.value)} placeholder='password...' required />
+                    <button onClick={Log()} className="btn btn primary">Login</button>
                 </form>
             </div>
         )
     }
-}
+
+
+
+    export default Login;
