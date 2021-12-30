9bf69fd77a66018af7d5c257cf3736a0d299f8d1
prajaktadanake <prajaktadanake@gmail.com>
2021-12-30 09:41:25 +0530

    update
    api

--- src/pages/componenets/usertable.jsx
+++ src/pages/componenets/usertable.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { DataGrid } from '@material-ui/data-grid'
 
-
+const UserTable = () => {
+  const [tableData, setTableData] = useState([])
+  useEffect(() => {
+    if(tableData=='')
+    {
+      fetch("http://localhost:8080/test/hello/getusers")
+    .then((data) => data.json())
+          .then((data) => setTableData(data))}
+        })
 const  HandleDelete=(id) =>{
   let url='http://localhost:8080/test/hello/delete/'+id;
   fetch(url)
   .then(resp=>resp.json())
   .then(resp=>UserTable())
 }
+const HandleUpdate=()=>{
+  let url='http://localhost:8080/test/hello/update';
+  fetch(url)
+  .then(resp=>resp.json())
+  .then(resp=>UserTable())
+}
+const [firstname, setID] = useState("")
 const columns = [
     { field: 'id', headerName: 'ID' },
     { field: 'firstname', headerName: 'First Name' },
@@ -23,36 +38,30 @@ const columns = [
         variant="contained"
         onClick={()=>HandleDelete(params.id)}
         >Delete</button>
-        
-       );
+ );
+       }
+    },
+    {field:'ActionUpdate',
+    headerName:'UpdateAction',
+    renderCell: () => {
+       return (
+        <button 
+        color= "primary"
+        variant="contained"
+        onClick={()=>HandleUpdate()}
+        >Update</button>
+ );
        }
     }
 ]
-const UserTable = () => {
-    const [tableData, setTableData] = useState([])
-    useEffect(() => {
-      if(tableData=='')
-      {
-        fetch("http://localhost:8080/test/hello/getusers")
-      
-            .then((data) => data.json())
-            .then((data) => setTableData(data))}
-          })
-    
-
-
-                    
-  
-
 return (
         <div style={{ height: 700, width: '100%' }}>
             <h2>This is user table</h2>
             <DataGrid
                 rows={tableData}
                 columns={columns}
-                
                 pageSize={10}
-                checkboxSelection /> ,  
+                checkboxSelection />  
                 
         </div>
       )
