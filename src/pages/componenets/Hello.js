fa1da93ae75c56577fcc71ed1e3189ec995483b6
Snehal11599 <snehalc11599@gmail.com>
2022-01-03 05:18:09 -0800

    react life cycle method

--- src/pages/componenets/Hello.js
+++ src/pages/componenets/Hello.js
@@ -1,25 +1,41 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-  constructor() {
-    super();
-    console.log(' constructor')
+    constructor() {
+        super();
+        this.state = { color: 'red' }
+        console.log(' constructor')
 
-  }
-  static getDerivedStateFromProps(){
-    console.log(' getDerivedStateFromProps')
-    return{};
+    }
+    static getDerivedStateFromProps() {
+        console.log(' getDerivedStateFromProps')
+        return {};
+    }
 
-}
- 
-componentDidMount(){
-    console.log(' componentDidMount')   
-}
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate ')
+        return true;
+    }
 
-  render() {
-    console.log(' render')
-    return (<p>Heloo</p>);
-  }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate', prevState.color)
+        return {};
+    }
+
+    componentDidUpdate() {
+        console.log('shouldComponentUpdate ')
+
+    }
+
+    componentDidMount() {
+        this.setState({ color: 'green' })
+        console.log(' componentDidMount')
+    }
+
+    render() {
+        console.log(' render', this.state)
+        return (<p>Heloo {this.state.color}</p>);
+    }
 }
 
 export default App;
