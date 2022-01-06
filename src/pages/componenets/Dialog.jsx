d73189e99a5b57f34256357f2877af13858dac28
Snehal11599 <snehalc11599@gmail.com>
2022-01-06 16:52:23 +0530

    Update Dialog.jsx

--- src/pages/componenets/Dialog.jsx
+++ src/pages/componenets/Dialog.jsx
@@ -1,37 +1,39 @@
-import   React from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-//import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import {TextField} from '@material-ui/core';
-
-export default function FormDialog({open , handleClose ,data ,onChange ,handleFormSubmit}) {
-  const {firstname, lastname,email,password }=data
+import { TextField } from '@material-ui/core';
+export default function FormDialog({ open, handleClose, data }) {
+  const { FirstName, LastName, Email, Password } = data
 
   return (
     <div>
-      
+
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description">
-      
-        { <DialogTitle id="alert-dialog-title">Create New User</DialogTitle>
-       }
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">"Create New User"</DialogTitle>
         <DialogContent>
           <form>
-            <TextField id ="firstname" value={firstname} onChange={e =>onChange(e)} placeholder='enter firstname' label ='FirstName' variant='outlined' margin='dense' fullWidth/>
-            <TextField id ="lastname" value={lastname} onChange={e =>onChange(e)} placeholder='enter lastname' label ='LastName'   variant='outlined' margin='dense' fullWidth/>
-            <TextField id ="email" value={email} onChange={e =>onChange(e)} placeholder='enter email' label ='Email'  variant='outlined' margin='dense'  fullWidth/>
-            <TextField id ="password" value={password} onChange={e =>onChange(e)}  placeholder='enter password' label ='Password'  variant='outlined' margin='dense' fullWidth/>
+            <TextField id="First Name" value={FirstName} placeholder="Enter First name" label="First Name" variant="filled" margin="dense" fullWidth />
+            <TextField id="Last Name" value={LastName} placeholder="EnterLast name" label="Last Name" variant="filled" margin="dense" fullWidth />
+            <TextField id="Email" value={Email} placeholder="Enter Email" label="Email" variant="filled" margin="dense" fullWidth />
+            <TextField id="Paswword" value={Password} placeholder="Enter Password" label="Password" variant="filled" margin="dense" fullWidth />
           </form>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} variant='outlined' >Cancel</Button>
-          <Button onClick={()=>handleFormSubmit()} variant='outlined' >Submit</Button>  
+          <Button onClick={handleClose} color="primary" variant="contained">
+            Cancel
+          </Button>
+          <Button color="secondary" variant="outlined" >
+            Submit
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
